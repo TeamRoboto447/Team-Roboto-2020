18ca51c0138e2c4458d0650b09edf11c3368e066
AdenKoperczak <38887432+AdenKoperczak@users.noreply.github.com>
2020-02-15 19:00:53 -0500

    refactoring PID constructer

--- 2020RobotCode/src/main/java/frc/robot/utils/PID.java
+++ 2020RobotCode/src/main/java/frc/robot/utils/PID.java
@@ -14,7 +14,7 @@ import frc.robot.utils.ff.FFbase;
  * Add your docs here.
  */
 public class PID {
-    double 
+    private double 
         integral, 
         previousError, 
         P, 
@@ -24,10 +24,11 @@ public class PID {
         maxInt,
         minInt,
         izone;
-    Timer 
+    private Timer 
         iterTime,
         integralTimer;
-    FFbase FF;
+    private FFbase FF;
+    private String name = "";
 
     public PID (double kSetpoint, double kP, double kI,
      double kD, FFbase kFF,
@@ -62,6 +63,21 @@ public class PID {
         this.maxInt=0;
         this.izone=0;        
     }
+    public PID (PIDBuilder builder){
+        this.P = builder.P;
+        this.I = builder.I;
+        this.D = builder.D;
+        this.FF = builder.kFF;
+        this.setpoint = builder.setpoint;
+        this.iterTime = new Timer();
+        this.iterTime.reset();
+        this.iterTime.stop();
+        this.integralTimer = new Timer();
+        this.integralTimer.reset();
+        this.minInt=builder.minInt;
+        this.maxInt=builder.maxInt;
+        this.izone=builder.izone;  
+    }
 
     public double run(double processingVar) {
         double iterTime = this.iterTime.get();
@@ -72,8 +88,6 @@ public class PID {
             this.resetIntegral();
             this.integralTimer.reset();
         }
-
-        Logging.info("PID time: "+iterTime,"PIDtime");
 
         final double error = this.setpoint - processingVar;
         if ( Math.abs(error) <= this.izone || this.izone==0){
@@ -88,8 +102,10 @@ public class PID {
         final double derivitive = (error - this.previousError) / iterTime;
         final double feedforward = this.FF.getFF(this.setpoint,processingVar,iterTime);
         
-        Logging.info("P:" + this.P*error + ",I:"+this.I*this.integral + ",D:"+this.D*derivitive,"PIDvals");
-
+        
+        Logging.info("PID time: "+iterTime,"PIDtime");
+        Logging.info("P:" + this.P*error + ",I:"+this.I*this.integral + ",D:"+this.D*derivitive+ ",FF:" + feedforward,"PIDOutVals");
+        
         this.previousError = error;
         return this.P*error + this.I*this.integral + this.D*derivitive + feedforward;
     }
@@ -102,41 +118,56 @@ public class PID {
         Logging.debug("Integral reset after val: "+this.integral, "shootIntegral");
     } 
     
-    public void updateP(double kP){
-        this.P = kP;
-    }
-
-    public void updateI(double kI){
-        this.I = kI;
-    }
-
-    public void updateD(double kD){
-        this.D = kD;
-    }
-
-    public void updateSetpoint(double kSetpoint){
-        this.setpoint = kSetpoint;
-    }
-
-    public double getP() {
-        return this.P;
-    }
-
-    public double getI() {
-        return this.I;
-    }
-
-    public double getD() {
-        return this.D;
-    }
+    public void updateP(double kP){ this.P = kP; }
+    public void updateI(double kI){ this.I = kI; }
+    public void updateD(double kD){ this.D = kD; }
+    public void updateSetpoint(double kSetpoint){ this.setpoint = kSetpoint; }
     
-    public double getSetpoint() {
-        return this.setpoint;
-    }
-    public void updateFF(FFbase kFF){
-        this.FF = kFF;
-    }
-    public FFbase getFF(){
-        return this.FF;
+    public double getP() { return this.P; }
+    public double getI() { return this.I; }
+
+    public double getD() { return this.D; }
+    
+    public double getSetpoint() { return this.setpoint; }
+    public void updateFF(FFbase kFF){ this.FF = kFF; }
+    public FFbase getFF(){ return this.FF; }
+
+    public class PIDBuilder {
+        double
+            P, 
+            I, 
+            D,
+            setpoint,
+            maxInt = 0,
+            minInt = 0,
+            izone = 0;
+        FFbase kFF = new FFbase();
+        String name = "";
+        public PIDBuilder (double kSetpoint, double kP, double kI, double kD){
+
+        }
+        public PID build(){
+            return new PID(this);
+        }
+        public PIDBuilder FF (FFbase kFF){
+            this.kFF = kFF;
+            return this;
+        }
+        public PIDBuilder MinIntegral(double minint){
+            this.minInt = minint;
+            return this;
+        }
+        public PIDBuilder MaxIntegeral(double maxint){
+            this.maxInt = maxint;
+            return this;
+        }
+        public PIDBuilder IZone(double kizone){
+            this.izone = kizone;
+            return this;
+        }
+        public PIDBuilder Name(String name){
+            this.name = name;
+            return this;
+        }
     }
 }
